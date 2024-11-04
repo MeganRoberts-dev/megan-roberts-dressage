bf07aeee40f5cc3d2047cbef661f10041496aa3b
Megan Roberts <megan.29@icloud.com>
2024-11-04 13:03:32 +0000

    Update static

--- home/views.py
+++ home/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
    # return HttpResponse ("This is my homepage (/)") #
-   return render(request, 'home.html')
+    context = {'name': 'Megan', 'job': 'Dressage Rider'}
+    return render(request, 'home.html', context)
 
 def about(request):
    # return HttpResponse ("This is my about page (/about)") #
@@ -11,7 +12,7 @@ def about(request):
 def services(request):
    # return HttpResponse ("This is my services page (/services)") #
     return render(request, 'services.html')
-    
+
 def contact(request):
   #  return HttpResponse ("This is my contact page (/contact)")  #
     return render(request, 'contact.html')
