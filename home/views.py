3c777034faa6efacd6704b75994748faab5256f9
Megan Roberts <megan.29@icloud.com>
2024-11-04 15:08:28 +0000

    Add new url path for files

--- home/views.py
+++ home/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render, HttpResponse
 def home(request):
    # return HttpResponse ("This is my homepage (/)") #
     context = {'name': 'Megan', 'job': 'Dressage Rider'}
-    return render(request, 'home.html', context)
+    return render(request, 'home/home.html', context)
 
 def about(request):
    # return HttpResponse ("This is my about page (/about)") #
-    return render(request, 'about.html')
+    return render(request, 'home/about.html')
 
 def services(request):
    # return HttpResponse ("This is my services page (/services)") #
-    return render(request, 'services.html')
+    return render(request, 'home/services.html')
 
 def contact(request):
   #  return HttpResponse ("This is my contact page (/contact)")  #
-    return render(request, 'contact.html')
+    return render(request, 'home/contact.html')
