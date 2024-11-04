1340bab6607b284b96ab2bed73d9e27ee44a1072
Megan Roberts <megan.29@icloud.com>
2024-11-04 10:24:02 +0000

    Create render request to each page with url

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('/', views.home, name="home")
+    path('', views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('services', views.services, name="services"),
+    path('contact', views.contact, name="contact"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
 def home(request):
-    return HttpResponse ("This is my homepage (/)")
+   # return HttpResponse ("This is my homepage (/)") #
+   return render(request, 'home.html')
+
+def about(request):
+   # return HttpResponse ("This is my about page (/about)") #
+    return render(request, 'about.html')
+
+def services(request):
+   # return HttpResponse ("This is my services page (/services)") #
+    return render(request, 'services.html')
+    
+def contact(request):
+  #  return HttpResponse ("This is my contact page (/contact)")  #
+    return render(request, 'contact.html')
