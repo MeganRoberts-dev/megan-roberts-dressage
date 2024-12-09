80fc9791ebbd6078133d64eabfaf782bdc73d188
Megan Roberts <megan.29@icloud.com>
2024-12-09 11:36:38 +0000

    Remove user folder

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.contrib.auth import views as auth_views
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('register/', views.register, name='register'),
-]
