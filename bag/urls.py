90cdfa9911eca4fd6f4466931910e8e5802bb20b
Megan Roberts <megan.29@icloud.com>
2024-12-09 11:12:47 +0000

    Remove bag folder

--- bag/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.view_bag, name='view_bag'),
-    path('add/<int:service_id>/', views.add_to_bag, name='add_to_bag'),
-    path('adjust/<int:service_id>/', views.adjust_bag, name='adjust_bag'),
-    path('remove/<int:service_id>/', views.remove_from_bag, name='remove_from_bag'),
-]
