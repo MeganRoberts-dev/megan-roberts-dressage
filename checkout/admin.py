35a4325a6b41905c47e2afc4efeccb14050d2e38
Megan Roberts <megan.29@icloud.com>
2025-04-17 15:28:49 +0000

    Update admin dashboard fields

--- checkout/admin.py
+++ checkout/admin.py
@@ -4,9 +4,10 @@ from .models import Order, Booking
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ("email", "full_name", "created_at", "total")
+    list_display = ("full_name", "purchaser", "email", "total", "created_at")
 
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ("date", "time")
+    list_filter = ["service"]
+    list_display = ("order", "service", "date", "time")
