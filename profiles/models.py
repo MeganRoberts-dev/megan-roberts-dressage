720e1bbffabd4a2db7bf7020a319da2131fbdde3
Megan Roberts <megan.29@icloud.com>
2025-04-17 17:10:33 +0000

    Add form for user to update profile

--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,25 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=75, null=False, blank=False)
+    last_name = models.CharField(max_length=75, null=False, blank=False)
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        # Safely get or create the profile
+        UserProfile.objects.get_or_create(user=instance)
